c2cd433f75daf9b2d1b5fef9c41adfa11c42a568
alexeidrummond <alexei.drummond@gmail.com>
2022-10-09 11:56:27 +1300

    updated to lphy 1.3.1

--- src/toroidalDiffusion/DihedralAngle3DViewer.java
+++ src/toroidalDiffusion/DihedralAngle3DViewer.java
@@ -14,9 +14,13 @@ public class DihedralAngle3DViewer extends JComponent {
     }
 
     public void paintComponent(Graphics g){
+
+        int width = getWidth();
+        int height = getHeight();
+
         Graphics2D g2D = (Graphics2D)g;
-        double x = 0;
-        double y = 0;
+        double x = width/2.0;
+        double y = height/2.0;
         for (Double[] anglePair : structure) {
             double phi = anglePair[0];
             double psi = anglePair[1];
@@ -24,6 +28,25 @@ public class DihedralAngle3DViewer extends JComponent {
             double ynew = y + Math.cos(phi) * bondLength;
             Line2D line = new Line2D.Double(x, y, xnew, ynew);
             g2D.draw(line);
+            x = xnew;
+            y = ynew;
         }
+    }
+
+    public static void main(String[] args ) {
+
+        Double[][] structure = new Double[20][2];
+
+        for (int i = 0; i < structure.length; i++) {
+            structure[i][0] = Math.random()*Math.PI-Math.PI/2.0;
+            structure[i][1] = Math.random()*Math.PI-Math.PI/2.0;
+        }
+
+        DihedralAngle3DViewer viewer = new DihedralAngle3DViewer(structure);
+
+        JFrame frame = new JFrame("DihedralAngle3DViewer");
+        frame.setSize(1000,800);
+        frame.getContentPane().add(viewer);
+        frame.setVisible(true);
     }
 }
