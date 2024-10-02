e56baa32af1041d9ff4f6fec5f78316f84d14bd8
clementineyan <clementineyan@gmail.com>
2024-10-02 16:27:05 +1300

    PhyloWrappedBivariateDiffusionToBeast - add internalnodes #6

--- torodiff-lphybeast/src/main/java/toroidaldiffusion/lphybeast/tobeast/generator/PhyloWrappedBivariateDiffusionToBeast.java
+++ torodiff-lphybeast/src/main/java/toroidaldiffusion/lphybeast/tobeast/generator/PhyloWrappedBivariateDiffusionToBeast.java
@@ -1,16 +1,23 @@
 package toroidaldiffusion.lphybeast.tobeast.generator;
 
 import beast.base.core.BEASTInterface;
+import beast.base.evolution.tree.TreeInterface;
 import beast.base.inference.parameter.RealParameter;
+import lphy.base.evolution.Taxa;
 import lphy.base.evolution.tree.TimeTree;
+import lphy.core.model.GraphicalModelNode;
 import lphy.core.model.Value;
 import lphybeast.BEASTContext;
 import lphybeast.GeneratorToBEAST;
 import org.forester.applications.phylo2coloredgraphics;
+import toroidaldiffusion.DihedralAngleAlignment;
 import toroidaldiffusion.PhyloWrappedBivariateDiffusion;
 import toroidaldiffusion.evolution.tree.DihedralAngleTreeModel;
+import toroidaldiffusion.lphybeast.tobeast.values.DihedralAnglesToBeast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class PhyloWrappedBivariateDiffusionToBeast implements GeneratorToBEAST<PhyloWrappedBivariateDiffusion, toroidaldiffusion.evolution.likelihood.PhyloWrappedBivariateDiffusion>{
 
@@ -18,27 +25,91 @@ public class PhyloWrappedBivariateDiffusionToBeast implements GeneratorToBEAST<P
     @Override
     public toroidaldiffusion.evolution.likelihood.PhyloWrappedBivariateDiffusion generatorToBEAST(PhyloWrappedBivariateDiffusion generator, BEASTInterface value, BEASTContext context) {
 
+        final RealParameter dihedralAngleAlignment;
+        if (value instanceof RealParameter realParameter) {
+            dihedralAngleAlignment = realParameter;
+        }
+        else {
+            throw new IllegalArgumentException ("not a RealParameter");
+        }
+
+
         toroidaldiffusion.evolution.likelihood.PhyloWrappedBivariateDiffusion phyloWrappedBivariateDiffusion = new toroidaldiffusion.evolution.likelihood.PhyloWrappedBivariateDiffusion();
 
         //create DA model
         DihedralAngleTreeModel dihedralAngleTreeModel = new DihedralAngleTreeModel();
 
         //get the time tree
-        Value<TimeTree> timeTreeValue = generator.getTree();
-        TimeTree timeTree = timeTreeValue.value();
+        //Value<TimeTree> timeTreeValue = generator.getTree();
+        TreeInterface timeTree = (TreeInterface) context.getBEASTObject(generator.getTree());
         dihedralAngleTreeModel.setInputValue("tree", timeTree);
 
         // Get the tip values (initial values for tips from the generator)
-        Value<Double[][]> tipValues = generator.getY();
-        Double[][] tipValuesArray = tipValues.value();
+       //y0 = root values, not tips value, need to figure out tipsvalues
+        //RealParameter tipValuesParam = context.getAsRealParameter(DihedralAnglesToBeast.);
 
-        RealParameter tipValuesParam = new RealParameter(Arrays.toString(tipValuesArray));
-        dihedralAngleTreeModel.setInputValue("tipValues", tipValuesParam);
+        //dihedralAngleTreeModel.setInputValue("tipValues", dihedralAngleAlignmentValue);
 
         //get internal nodes values
 //        Double[][] internalNodesValuesArray = generator.getinternalNodes;
 //        RealParameter internalNodesValuesParam = new RealParameter(internalNodesValuesArray);
 //        dihedralAngleTreeModel.setInternalNodesValuesParam(internalNodesValuesParam);
+
+        // int pairsLength = 2 * dihedralAngleAlignment.getTaxa().ntaxa() - 1;
+        // get the num of taxa
+
+        //get the num of taxa from values
+        //num of sites = length of y0 array
+        Value<Double[][]> array = generator.getY();
+        int site = array.value().length;
+        int taxa = dihedralAngleAlignment.getDimension()/site/2;
+
+        //get the actual num of taxa
+        DihedralAngleAlignment dihedralAngleAlignmentValue = (DihedralAngleAlignment) value; //TODO: can we cast realparameter -> DihedralAngleAlignment?
+        String [] taxaNames = dihedralAngleAlignmentValue.getTaxa().getTaxaNames();
+
+        if (taxa == taxaNames.length) {
+            dihedralAngleTreeModel.setInputValue("tipValues", dihedralAngleAlignment);
+        }
+//        else if (taxa > taxaNames.length) {
+//            List<Double> tipsValues = new ArrayList<>();
+//            //num of tips angles = taxon * site * 2 (pairs)
+//            for (int i = 0; i < taxaNames.length*site*2 - 1; i++) {
+//                double angle = dihedralAngleAlignment.getValue(i);
+//                tipsValues.add(angle);
+//            }
+//            List<Double> internalNodes = new ArrayList<>();
+//            //values beyond the tip values are internal node values
+//            for (int i = taxaNames.length*site*2; i < (dihedralAngleAlignment.getDimension() - 1); i++) {
+//                double angle = dihedralAngleAlignment.getValue(i);
+//                internalNodes.add(angle);
+//            }
+//
+//            dihedralAngleTreeModel.setInputValue("tipValues", context.getAsRealParameter((Value) tipsValues));
+//            dihedralAngleTreeModel.setInputValue("internalNodesAngles", context.getAsRealParameter((Value) internalNodes));
+//
+//            //todo: how to keep dihedralAngleAlignment as input but change its value to keep only tipsangles
+//            //todo: context.addBEASTObject() required inputs are the arguments of a function or distribution or the function/distribution
+//        }
+
+        else if (taxa < taxaNames.length) {
+            for (int i = 0; i < taxa; i++) {
+                double angle = dihedralAngleAlignment.getValue(i);
+                dihedralAngleAlignment.setValue(i, angle);
+            }
+
+            RealParameter internalnodes = new RealParameter();
+            for (int i = taxaNames.length * site * 2; i < dihedralAngleAlignment.getDimension(); i++) {
+                double angle = dihedralAngleAlignment.getValue(i);
+                internalnodes.setValue(i, angle);
+            }
+            dihedralAngleTreeModel.setInputValue("tipValues", dihedralAngleAlignment);
+            context.addBEASTObject(dihedralAngleTreeModel, (GraphicalModelNode) internalnodes);
+            //todo: context.addBEASTObject() required inputs are the arguments of a function or distribution or the function/distribution
+        }
+        else {
+            throw new IllegalStateException("Unexpected condition: 'taxa' is less than expected or invalid.");
+        }
 
         phyloWrappedBivariateDiffusion.setInputValue("daTreeModel", dihedralAngleTreeModel);
         phyloWrappedBivariateDiffusion.setInputValue("mu", context.getAsRealParameter(generator.getParams().get("mu")));
