953d42e3727535963cb67b0092e2a76d41234022
Walter <walterxie@users.noreply.github.com>
2024-11-25 14:13:03 +1300

    replicate the problem #8

---
+++ torodiff-lphy/src/test/java/toroidaldiffusion/WrappedBivariateDiffusionTest.java
@@ -0,0 +1,34 @@
+package toroidaldiffusion;
+
+import org.junit.jupiter.api.Test;
+
+class WrappedBivariateDiffusionTest {
+
+    final double[] mu = new double[]{4.0509262973708315, 5.1236852445169685};
+    final double[] sigma = new double[]{1.0298711760259887, 0.1271923963524306};
+    final double[] alpha = new double[]{0.75761827331078, 0.8251637431436857, 0.14751750525982674};
+
+
+    @Test
+    void testLikelihood() {
+        WrappedBivariateDiffusion diffusion = new WrappedBivariateDiffusion();
+
+        diffusion.setParameters(mu, alpha, sigma);
+        double t = 0.04184533374639288;
+        diffusion.setParameters(t);
+
+        // TODO: mu too high ?
+        /**
+         * org.apache.commons.math3.exception.MathIllegalStateException: trust region step has failed to reduce Q
+         * 	at commons.math3@3.6.1/org.apache.commons.math3.optim.nonlinear.scalar.noderiv.BOBYQAOptimizer.bobyqb(BOBYQAOptimizer.java:880)
+         * 	at commons.math3@3.6.1/org.apache.commons.math3.optim.nonlinear.scalar.noderiv.BOBYQAOptimizer.bobyqa(BOBYQAOptimizer.java:340)
+         * 	at commons.math3@3.6.1/org.apache.commons.math3.optim.nonlinear.scalar.noderiv.BOBYQAOptimizer.doOptimize(BOBYQAOptimizer.java:252)
+         * 	at commons.math3@3.6.1/org.apache.commons.math3.optim.nonlinear.scalar.noderiv.BOBYQAOptimizer.doOptimize(BOBYQAOptimizer.java:49)
+         * 	at commons.math3@3.6.1/org.apache.commons.math3.optim.BaseOptimizer.optimize(BaseOptimizer.java:153)
+         * 	at commons.math3@3.6.1/org.apache.commons.math3.optim.BaseMultivariateOptimizer.optimize(BaseMultivariateOptimizer.java:65)
+         * 	at commons.math3@3.6.1/org.apache.commons.math3.optim.nonlinear.scalar.MultivariateOptimizer.optimize(MultivariateOptimizer.java:63)
+         */
+        double[][] samples = diffusion.sampleByRejection(3.0875357245670547, 3.2389546454267055, 1);
+
+    }
+}
