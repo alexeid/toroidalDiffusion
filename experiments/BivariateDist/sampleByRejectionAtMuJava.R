a3165def1581423ee2dc04e3a5a06a77eb61c719
Walter <walterxie@users.noreply.github.com>
2025-01-21 11:56:42 +1300

    print angle range #9 #11

--- experiments/BivariateDist/sampleByRejectionAtMuJava.R
+++ experiments/BivariateDist/sampleByRejectionAtMuJava.R
@@ -4,8 +4,6 @@ setwd("~/WorkSpace/toroidalDiffusion/experiments/BivariateDist")
 
 library(tidyverse)
 
-angles <- read_delim("sampleByRejectionAtMu.txt", delim = "\t");
-
 # contour plot for bivariate normal data
 contour <- function(bvn, x_string, y_string, size, alpha){
   require("ggplot2")
@@ -23,6 +21,9 @@ mu1 <- pi * 0.65; s1 <- 1.5
 mu2 <- pi * 0.8; s2 <- 1.5
 alpha <- c(1.0, 0.5, 0.5)
 
+angles <- read_delim("sampleByRejectionAtMu.txt", delim = "\t");
+range(angles[,1:2])
+
 N <- nrow(angles)
 p1 <- contour(angles[,1:2], "phi","psi", size=psize, alpha=palpha)
 p1
@@ -30,6 +31,7 @@ ggsave("sampleByRejectionAtMuJava.pdf", p1, width = 6, height = 6)
 
 
 angles2 <- read_delim("WrappedBivariateNormalJava.txt", delim = "\t");
+range(angles2[,1:2])
 
 N <- nrow(angles2)
 p2 <- contour(angles2[,1:2], "phi","psi", size=psize, alpha=palpha)
